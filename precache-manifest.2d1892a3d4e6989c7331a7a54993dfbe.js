8cb1a27ca0c68031ce2a6ca2c0f06bacdff8bf2a
Mike Upton <uptonm@wit.edu>
2019-09-16 06:37:18 -0400

    Updates

---
+++ precache-manifest.2d1892a3d4e6989c7331a7a54993dfbe.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2593721b398cdcc2a0437341b116496",
+    "url": "/hackwitus-v2/index.html"
+  },
+  {
+    "revision": "e79213ac617238dadd9d",
+    "url": "/hackwitus-v2/static/css/main.85021d7f.chunk.css"
+  },
+  {
+    "revision": "5951247643b8273c0007",
+    "url": "/hackwitus-v2/static/js/2.1d453e62.chunk.js"
+  },
+  {
+    "revision": "e79213ac617238dadd9d",
+    "url": "/hackwitus-v2/static/js/main.6243f02b.chunk.js"
+  },
+  {
+    "revision": "bc5a0dc6d3c5946533c3",
+    "url": "/hackwitus-v2/static/js/runtime~main.a84bb3f3.js"
+  },
+  {
+    "revision": "eaabc14ef8ce877de8b650524aaa53c1",
+    "url": "/hackwitus-v2/static/media/accelerate-logo.eaabc14e.png"
+  },
+  {
+    "revision": "a1b52f11870b010f1f3fd0ff145b4154",
+    "url": "/hackwitus-v2/static/media/coops-careers-logo.a1b52f11.png"
+  },
+  {
+    "revision": "033ebc5c88d8d293e10510bca2946a66",
+    "url": "/hackwitus-v2/static/media/hackwit-text.033ebc5c.svg"
+  },
+  {
+    "revision": "3a0b0e1733f83f5bfe3d17bdfb8497e9",
+    "url": "/hackwitus-v2/static/media/hackwitus-organizers.3a0b0e17.jpg"
+  },
+  {
+    "revision": "0e4394b1d23b076fbcf25ae4aa63313b",
+    "url": "/hackwitus-v2/static/media/leologo.0e4394b1.svg"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hackwitus-v2/precache-manifest.2d1892a3d4e6989c7331a7a54993dfbe.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hackwitus-v2/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
