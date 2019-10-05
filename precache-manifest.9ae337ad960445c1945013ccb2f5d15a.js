6665cd62746c828aed6d5aa1ee2b258b4bd5d858
Mike Upton <uptonm@wit.edu>
2019-10-05 09:06:33 -0400

    Updates

---
+++ precache-manifest.9ae337ad960445c1945013ccb2f5d15a.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9ef4d5a30f8d71ff179f2b00c6b7d5a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4175cda93a43c2bfb81c",
+    "url": "/static/css/main.2e845690.chunk.css"
+  },
+  {
+    "revision": "043e7af710b9c5e8654c",
+    "url": "/static/js/2.4b2ffced.chunk.js"
+  },
+  {
+    "revision": "4175cda93a43c2bfb81c",
+    "url": "/static/js/main.40eeb6fc.chunk.js"
+  },
+  {
+    "revision": "b3938888db487c1703c6",
+    "url": "/static/js/runtime~main.a7ca617d.js"
+  },
+  {
+    "revision": "eaabc14ef8ce877de8b650524aaa53c1",
+    "url": "/static/media/accelerate-logo.eaabc14e.png"
+  },
+  {
+    "revision": "8b6fbb5adc10d5ae36ba4550d287faa6",
+    "url": "/static/media/comp-sci-dept-logo.8b6fbb5a.jpg"
+  },
+  {
+    "revision": "a1b52f11870b010f1f3fd0ff145b4154",
+    "url": "/static/media/coops-careers-logo.a1b52f11.png"
+  },
+  {
+    "revision": "cefc20232703e5e3c24efd5f50d75e26",
+    "url": "/static/media/github-logo.cefc2023.png"
+  },
+  {
+    "revision": "033ebc5c88d8d293e10510bca2946a66",
+    "url": "/static/media/hackwit-text.033ebc5c.svg"
+  },
+  {
+    "revision": "3e67447e4ef6bedc247394c59cfe0f53",
+    "url": "/static/media/hackwitus-keynote.3e67447e.jpg"
+  },
+  {
+    "revision": "3a0b0e1733f83f5bfe3d17bdfb8497e9",
+    "url": "/static/media/hackwitus-organizers.3a0b0e17.jpg"
+  },
+  {
+    "revision": "faa3372e19e5342b11ae3493c25818fe",
+    "url": "/static/media/hackwitus-project-teams.faa3372e.jpg"
+  },
+  {
+    "revision": "f434a5c89b60258b52bc34154ff54fe9",
+    "url": "/static/media/leologo-white.f434a5c8.svg"
+  },
+  {
+    "revision": "95a0b2bb98d008ae17eb099cf7abdf71",
+    "url": "/static/media/particle-io-logo.95a0b2bb.png"
+  },
+  {
+    "revision": "fc3f2afc3e6ae89d0f0716e52404323d",
+    "url": "/static/media/sketch-logo.fc3f2afc.png"
+  },
+  {
+    "revision": "6ae25835a064c54e5d91edc84abc5df9",
+    "url": "/static/media/wit-leadership-logo.6ae25835.png"
+  }
+]);
